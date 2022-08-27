5a1628d8928e92331da7ceb1a14373588b849da5
Satoe Sakonjo <sakonjo310@gmail.com>
2022-08-27 23:03:14 +0000

    Add tinkering.js file

---
+++ tinkering.js
@@ -0,0 +1,31 @@
+/*function multiply(a, b) {
+  return a * b;
+}
+
+const result = multiply(2, 4);
+
+console.log(result);
+
+// Pick a string. Your string can have any number of characters.
+var my_string = "a";
+
+// Calculate the ASCII value of the first character, i.e. the character at the position 0. 
+var ASCII_value = my_string.charCodeAt(0);
+
+// Let us print
+console.log(ASCII_value);
+
+var my_string = "Udacity";
+
+// Iterate using a Loop
+for (var i = 0; i < my_string.length; i++) {
+  console.log(my_string.charCodeAt(i));
+}*/
+
+let price = 15;
+let money = 10;
+if (price <= money) {
+  console.log("buy the stuff");
+} else {
+  console.log("don't buy the stuff");
+}
