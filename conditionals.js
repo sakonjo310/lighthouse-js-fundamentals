172fde0d36db00528849dca3a2343aaaf3cd99d6
Satoe Sakonjo <sakonjo310@gmail.com>
2022-08-27 23:01:13 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -15,7 +15,7 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 */
 
-const temp = 16;
+const temp = 41;
 
 if (temp < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -26,3 +26,18 @@ if (temp < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 20;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+} 
+
+if (temp < -40 || temp > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
